0a2bf05f84f96004d39a9f18d03ab674e34bb17f
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2020-11-21 12:58:02 +0530

    block-2 of mongoDB

---
+++ block-BMaaab/db.js
@@ -0,0 +1,13 @@
+//code to create a database of your country name.
+
+        // db.countryName.createOne(
+        //   {
+        //       "countryName":"India"
+        //   }  
+        // )
+
+
+// code to check list of databases to see newly created database.
+
+    // db.countryName.find()
+
